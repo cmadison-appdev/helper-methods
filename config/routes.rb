cff1954884d3e88af9e567e23218ffbce4f9cd58
chrismad26 <cmadison@chicagobooth.edu>
2021-04-21 01:03:42 +0000

    refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,28 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "movies", :action => "index" })
+  
+  root "movies#index"
 
-  # Routes for the Movie resource:
-
-  # CREATE
-  post("/movies", { :controller => "movies", :action => "create" })
-  get("/movies/new", { :controller => "movies", :action => "new" })
+  
+# CREATE
+  
+  post "/movies" => "movies#create"
+  
+  get "/movies/new" => "movies#new"
           
-  # READ
-  get("/movies", { :controller => "movies", :action => "index" })
-  get("/movies/:id", { :controller => "movies", :action => "show" })
+ # READ 
+  get "/movies" => "movies#index"
   
-  # UPDATE
-  patch("/movies/:id", { :controller => "movies", :action => "update" })
-  get("/movies/:id/edit", { :controller => "movies", :action => "edit" })
+  get "/movies/:id" => "movies#show"
   
-  # DELETE
-  delete("/movies/:id", { :controller => "movies", :action => "destroy" })
+  
+ # UPDATE 
+  patch "/movies/:id" => "movies#update"
+  
+  get "/movies/:id/edit" => "movies#edit"
+  
+  
+# DELETE
+  delete "/movies/:id" => "movies#destroy"
 
   #------------------------------
 end
