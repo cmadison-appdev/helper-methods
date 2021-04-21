cb7a5672f7dc500f552acfb2599b8bb75da46c2f
chrismad26 <cmadison@chicagobooth.edu>
2021-04-21 02:22:58 +0000

    about to re-factor old syntax

--- config/routes.rb
+++ config/routes.rb
@@ -4,21 +4,18 @@ Rails.application.routes.draw do
 
   
 # CREATE
-  
-  post "/movies" => "movies#create"
-  
-  get "/movies/new" => "movies#new"
+  post "/movies" => "movies#create", as: :movies #movies_url and movies_path
+  get "/movies/new" => "movies#new", as: :new_movie
           
  # READ 
   get "/movies" => "movies#index"
-  
-  get "/movies/:id" => "movies#show"
+  get "/movies/:id" => "movies#show", as: :movie #movie_path() (expects an arg to populate ID segment)
   
   
  # UPDATE 
   patch "/movies/:id" => "movies#update"
   
-  get "/movies/:id/edit" => "movies#edit"
+  get "/movies/:id/edit" => "movies#edit", as: :edit_movie
   
   
 # DELETE
